7d50b5d01d9fdf046d27664892479dee177f47c6
Ron <aarontalua14@gmail.com>
2023-06-14 07:32:09 +0800

    lol

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import Todo from "./modules/todo.js";
 import Project from "./modules/project.js";
 import Logic from "./modules/logicController.js";
 import Display from "./modules/displayController.js";
+
+// Do I even need this? Nope. DisplayController does it all.
